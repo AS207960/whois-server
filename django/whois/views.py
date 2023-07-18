87621ce2093b29682f0a6abeab06c5c87d11fcf8
Q <q@misell.cymru>
2023-07-18 10:08:26 +0100

    fix org units

--- django/whois/views.py
+++ django/whois/views.py
@@ -700,7 +700,7 @@ def map_js_card(card: rdap_pb2.JSCard):
 
         if v.HasField("name"):
             card_dict["organizations"][k]["name"] = v.name.value
-        if v.HasField("units"):
+        if v.units:
             card_dict["organizations"][k]["units"] = [map_org_unit(u) for u in v.units]
         if v.HasField("sort_an"):
             card_dict["organizations"][k]["sortAs"] = v.sort_an.value
